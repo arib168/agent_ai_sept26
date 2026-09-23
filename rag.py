b98f5cbabfea5b94bfc08266690f7f61d7544c3d
Arib Nawal <65786174+arib168@users.noreply.github.com>
2026-09-23 09:54:40 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -1,5 +1,4 @@
-import warnings 
-warnings.filterwarnings("ignore",category=DeprecationWarning)
+#RAG Agent code
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
